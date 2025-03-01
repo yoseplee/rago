037262677e5466d993fc25b83a9cad5bd501b589
yoseplee <yoseplee@linecorp.com>
2025-03-01 20:47:06 +0900

    add chat completion

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,25 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/openai/openai-go"
 	"github.com/yoseplee/rago"
 	"github.com/yoseplee/rago/infra"
 )
 
 func main() {
 	defer infra.Logger.Sync()
+	retrieve()
+}
 
-	ingester := rago.NewDefaultIngester(rago.JSONLoader{FilePath: "sample_shop_items_all.json"})
-	if err := ingester.Ingest(); err != nil {
+func retrieve() {
+	retriever := rago.DefaultRetriever{}
+	if err := retriever.Retrieve(); err != nil {
 		panic(err)
 	}
 }
 
-func chatCompletionExample() {
-	if chatCompletion, err := infra.OpenAIClient.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
-		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-			openai.UserMessage("Describe who you are."),
-		}),
-		Model: openai.F(openai.ChatModelGPT3_5Turbo),
-	}); err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Println(chatCompletion.Choices[0].Message.Content)
+func ingest() {
+	ingester := rago.NewDefaultIngester(rago.JSONLoader{FilePath: "sample_shop_items_all.json"})
+	if err := ingester.Ingest(); err != nil {
+		panic(err)
 	}
 }
