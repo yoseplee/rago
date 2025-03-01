2055d9019cf2581efbb74133914ef71c9b7d555a
yoseplee <yoseplee@linecorp.com>
2025-03-01 23:58:55 +0900

    add errors

--- v2/ingester.go
+++ v2/ingester.go
@@ -1,5 +1,14 @@
 package v2
 
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	IngestErr = errors.New("error occurred while ingesting documents")
+)
+
 // Ingester is responsible for loading, modifying, embedding-generating, and storing to Vector Search Engine.
 type Ingester interface {
 	Ingest() error
@@ -16,21 +25,21 @@ type DefaultIngester struct {
 func (d DefaultIngester) Ingest() error {
 	loadedDocuments, loadErr := d.DocumentLoader.Load()
 	if loadErr != nil {
-		return loadErr
+		return fmt.Errorf("%w: %v", IngestErr, loadErr)
 	}
 
-	modifiedDocuments, err := d.DocumentModifiers.ApplyAll(loadedDocuments)
-	if err != nil {
-		return err
+	modifiedDocuments, modifyErr := d.DocumentModifiers.ApplyAll(loadedDocuments)
+	if modifyErr != nil {
+		return fmt.Errorf("%w: %v", IngestErr, modifyErr)
 	}
 
 	embeddings, embeddingGenerateErr := d.EmbeddingGenerator.Generate(modifiedDocuments)
 	if embeddingGenerateErr != nil {
-		return embeddingGenerateErr
+		return fmt.Errorf("%w: %v", IngestErr, embeddingGenerateErr)
 	}
 
 	if storeErr := d.KnowledgeAddable.Add(d.CollectionName, embeddings); storeErr != nil {
-		return storeErr
+		return fmt.Errorf("%w: %v", IngestErr, storeErr)
 	}
 
 	return nil
