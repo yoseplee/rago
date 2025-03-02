61392d2dde50c38b7a19e95c1ac906d17c32586d
yoseplee <yoseplee@linecorp.com>
2025-03-03 07:53:37 +0900

    implement json document loader

--- v2/document_loader.go
+++ v2/document_loader.go
@@ -1,6 +1,38 @@
 package v2
 
+import (
+	"encoding/json"
+	"os"
+)
+
 // DocumentLoader is responsible for loading data from a specific type of source.
 type DocumentLoader interface {
 	Load() (Documents, error)
+}
+
+type JSONDocumentLoader struct {
+	FilePath string
+}
+
+func (j JSONDocumentLoader) Load() (Documents, error) {
+	file, readFileErr := os.ReadFile(j.FilePath)
+	if readFileErr != nil {
+		return nil, readFileErr
+	}
+
+	var unmarshalledObjects []interface{}
+	if unmarshalErr := json.Unmarshal(file, &unmarshalledObjects); unmarshalErr != nil {
+		return nil, unmarshalErr
+	}
+
+	var documents Documents
+	for _, unmarshalledObject := range unmarshalledObjects {
+		marshalledObject, marshalErr := json.Marshal(unmarshalledObject)
+		if marshalErr != nil {
+			return nil, marshalErr
+		}
+		documents = append(documents, Document(marshalledObject))
+	}
+
+	return documents, nil
 }
