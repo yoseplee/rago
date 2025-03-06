638d4c6575d593ed7e12e2c8991bcaf4133f6e4e
yoseplee <yoseplee@linecorp.com>
2025-03-06 19:37:23 +0900

    refactor: streamline document retrieval logic in api.go for improved readability

--- internal/api/api.go
+++ internal/api/api.go
@@ -81,65 +81,61 @@ func Retrieve(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, CommonResponse{Message: err.Error()})
 	}
 
-	if false {
-		retriever := v1.DefaultRetriever{
-			TopK: config.Config.Retrievers["default"].KnowledgeBaseSearch.TopK,
-			EmbeddingGenerator: v1.OpenAIEmbeddingGenerator{
-				ModelName:            v1.ModelName(config.Config.Retrievers["default"].EmbeddingGenerator.Model),
-				Dimension:            v1.Dimension(config.Config.Retrievers["default"].EmbeddingGenerator.Dimension),
-				EmbeddingGeneratable: OpenAIClient,
-			},
-			KnowledgeSearchable: v1.OpenSearchKnowledgeBase{
-				CollectionName:  indexName,
-				Indexable:       opensearch.GetClient(),
-				IndexSearchable: opensearch.GetClient(),
-			},
-		}
-
-		items := []v1.Document{v1.Document("")}
-
-		retrieved, err := retriever.Retrieve(items)
-		if err != nil {
-			logger.Error(
-				"failed to retrieve documents",
-				[]logger.F[any]{
-					{
-						"err",
-						err.Error(),
-					},
-				},
-			)
-			return c.JSON(http.StatusInternalServerError, CommonResponse{Message: err.Error()})
-		}
-
-		var chatCompletions []string
-		for i, result := range retrieved {
-			documents := result.Documents()
-			scores := result.Scores()
-
-			chatCompletion, err := LinecorpOpenAIClient.Client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
-				Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-					openai.UserMessage(fmt.Sprintf(
-						"Here are some context documents retrieved from our Vector Database: [%+v]. These documents are potential candidates. Each candidate has a relevance score between 0 and 1: [%+v].",
-						documents,
-						scores,
-					)),
-					openai.UserMessage(fmt.Sprintf("Please suggest the best alternative for the item [%s].", items[i])),
-					openai.UserMessage("Follow these instructions carefully: Provide the name of the recommended item, explain the reason for each suggestion (do not mention the score), and list up to 5 items as alternatives."),
-					openai.UserMessage("You must exclude the given item from the alternatives."),
-					openai.UserMessage("Note that the user may not be satisfied with your suggestions. Please be cautious with your recommendations."),
-				}),
-				Model: openai.F(openai.ChatModelGPT4o),
-			})
-			if err != nil {
-				panic(err.Error())
-			}
-
-			chatCompletions = append(chatCompletions, chatCompletion.Choices[0].Message.Content)
-		}
-
-		return c.JSON(http.StatusOK, CommonResponse{Message: fmt.Sprintf("%v", chatCompletions)})
+	retriever := v1.DefaultRetriever{
+		TopK: config.Config.Retrievers["default"].KnowledgeBaseSearch.TopK,
+		EmbeddingGenerator: v1.OpenAIEmbeddingGenerator{
+			ModelName:            v1.ModelName(config.Config.Retrievers["default"].EmbeddingGenerator.Model),
+			Dimension:            v1.Dimension(config.Config.Retrievers["default"].EmbeddingGenerator.Dimension),
+			EmbeddingGeneratable: OpenAIClient,
+		},
+		KnowledgeSearchable: v1.OpenSearchKnowledgeBase{
+			CollectionName:  indexName,
+			Indexable:       opensearch.GetClient(),
+			IndexSearchable: opensearch.GetClient(),
+		},
 	}
 
-	return c.JSON(http.StatusOK, CommonResponse{Message: "success"})
+	items := []v1.Document{v1.Document("")}
+
+	retrieved, err := retriever.Retrieve(items)
+	if err != nil {
+		logger.Error(
+			"failed to retrieve documents",
+			[]logger.F[any]{
+				{
+					"err",
+					err.Error(),
+				},
+			},
+		)
+		return c.JSON(http.StatusInternalServerError, CommonResponse{Message: err.Error()})
+	}
+
+	var chatCompletions []string
+	for i, result := range retrieved {
+		documents := result.Documents()
+		scores := result.Scores()
+
+		chatCompletion, err := LinecorpOpenAIClient.Client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+			Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+				openai.UserMessage(fmt.Sprintf(
+					"Here are some context documents retrieved from our Vector Database: [%+v]. These documents are potential candidates. Each candidate has a relevance score between 0 and 1: [%+v].",
+					documents,
+					scores,
+				)),
+				openai.UserMessage(fmt.Sprintf("Please suggest the best alternative for the item [%s].", items[i])),
+				openai.UserMessage("Follow these instructions carefully: Provide the name of the recommended item, explain the reason for each suggestion (do not mention the score), and list up to 5 items as alternatives."),
+				openai.UserMessage("You must exclude the given item from the alternatives."),
+				openai.UserMessage("Note that the user may not be satisfied with your suggestions. Please be cautious with your recommendations."),
+			}),
+			Model: openai.F(openai.ChatModelGPT4o),
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		chatCompletions = append(chatCompletions, chatCompletion.Choices[0].Message.Content)
+	}
+
+	return c.JSON(http.StatusOK, CommonResponse{Message: fmt.Sprintf("%v", chatCompletions)})
 }
